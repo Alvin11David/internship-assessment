bf0b842e45941d182c9adbefa1f54e28db7f6352
Alvin11David <alvin69david@gmail.com>
2026-05-10 14:04:14 +0300

    Added the css changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-
-
+import "./globals.css";
 export const metadata: Metadata = {
   title: "Sunbird AI GenAI App",
   description: "Text/Audio Processing with AI Summarization & Translation",
